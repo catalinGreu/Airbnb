3f8ba6e166a17214fb125d7d7b62bc74d3a495ce
catalinGreu <catalin_greu@hotmail.com>
2016-02-03 14:38:54 +0100

    EN proceso <Task>ActionResult

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Threading.Tasks;
+using Proyecto_AirBnb.Models;
 namespace Proyecto_AirBnb.Controllers
 {
     public class AccountController : Controller
@@ -12,6 +13,22 @@ namespace Proyecto_AirBnb.Controllers
         public ActionResult Registro()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registro(RegistroViewModel model) // va a tener que ser si o si un hilo TASK
+        {
+            if (ModelState.IsValid)
+            {
+                //hago algo y grabo el usuario
+
+                return RedirectToAction("Index", "Inicio");
+            }
+            
+
+            return View(model);
         }
     }
 }
