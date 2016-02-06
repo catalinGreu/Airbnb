ad463602ba196c022a0d9609cb2b3543eb2ee8e2
catalinGreu <catalin_greu@hotmail.com>
2016-02-06 02:41:14 +0100

    Subir anuncios progreso

---
+++ Controllers/AnunciosController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_AirBnb.Models;
+namespace Proyecto_AirBnb.Controllers
+{
+    public class AnunciosController : Controller
+    {
+        // GET: Anuncios
+        public ActionResult NuevoAnuncio()
+        {
+            CreaAnuncioViewModel model = new CreaAnuncioViewModel();
+            model.Alojamientos = new List<string> { "Apartamento", "Casa", "Bed&Breakfast" };
+            model.Habitaciones = new List<string> { "Casa/Apto. Entero", "Habitacion privada", "Habitacion compartida" };
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult NuevoAnuncio(CreaAnuncioViewModel model)
+        {
+            // SI el usuario no esta conectado no puede subir anuncios
+
+            if (ModelState.IsValid)
+            {
+                if (Session["usuario"] != null)
+                {
+                    //grabo el anuncio y voy a completar anuncio
+
+
+                }
+                else
+                {
+                    //le mando a registro o a login...y guardo en sesion el Anuncio (Incompleto)
+                    Anuncio a = new Anuncio { Alojamiento = model.Alojamiento, Habitacion = model.Habitacion, Capacidad = model.Capacidad, Localidad = model.Ciudad };
+                    Session["anuncio"] = a;
+                    // AUN NO GRABO EN BD, HASTA QUE NO SE REGISTRE O LOGGEE
+                    // PERO METO EN SESSION EL Anuncio POR AHORA
+                    ViewBag.Warning = "Registrate o Inicia sesión para continuar";
+                    return RedirectToAction("NuevoAnuncio", "Anuncios");
+                }
+            }
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region "Accesso a datos"
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Models/AnunciosViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_AirBnb.Models
+{
+    public class CreaAnuncioViewModel
+    {
+        [Required(ErrorMessage ="Selecciona tipo de alojamiento")]
+        [Display(Name = "Tipo de alojamiento")]
+        public string Alojamiento { get; set; }
+        [Required(ErrorMessage ="Selecciona tipo de habitación")]
+        [Display(Name = "Tipo de habitación")]
+        public string Habitacion { get; set; }
+        [Required(ErrorMessage ="Selecciona capacidad")]
+        [Display(Name = "Capacidad")]
+        [Range(1,16,ErrorMessage = "Huéspedes entre 1 y 16")]
+        public int Capacidad { get; set; }
+        [Required(ErrorMessage ="Escoge ciudad")]
+        [Display(Name = "Ciudad")]
+        public string Ciudad { get; set; } 
+        public List<string> Alojamientos { get; set; }
+        public List<string> Habitaciones { get; set; }
+
+    }
+
+    //hacer otro que sea: Completa Anuncio
+
+}
